6532fa8a7ed34dd1bc7e9199e335fb68223b0b28
Johan Eriksson <imoxus@gmail.com>
2018-12-02 18:54:26 +0100

    day2part1

--- src/day2.py
+++ src/day2.py
@@ -1,9 +1,18 @@
+import string
+
 def solvepart1():
-	freq = 0
-	with open('inputs/day1.txt') as f:
-		for i in f:
-			freq += int(i)
-	return freq
+	counts = [0, 0]
+	with open('inputs/day2.txt') as f:
+		for line in f:
+			for i in [2,3]:
+				for c in string.ascii_lowercase:
+					if(string.count(line, c)==i):
+						counts[2-i] += 1
+						break
+
+	return counts[0]*counts[1]
+
+
 
 def solvepart2():
 	freq = 0
