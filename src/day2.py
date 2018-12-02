13f0c22230dfa1738a617fcb12f33430b02e7c77
Johan Eriksson <imoxus@gmail.com>
2018-12-02 20:56:32 +0100

    day 2 done

--- src/day2.py
+++ src/day2.py
@@ -12,20 +12,32 @@ def solvepart1():
 
 	return counts[0]*counts[1]
 
-
-
 def solvepart2():
-	freq = 0
-	seen_freqs = set([freq])
-	while True:
-		with open('inputs/day1.txt') as f:
-			for i in f:
-				freq += int(i)
-				if freq in seen_freqs:
-					return freq
-				seen_freqs.add(freq)
-
-	return freq
+	with open('inputs/day2.txt') as f:
+		inputs = f.readlines()
+		
+	for line1_index in range(len(inputs)):
+		distance = [0 for _ in range(len(inputs))]
+		l1 = inputs[line1_index]
+		for line2_index in range(len(inputs)):
+			l2 = inputs[line2_index]
+			for i in range(len(inputs[0])): #every input has the same length
+				if l1==l2:
+					distance[line2_index] = 20 #dummy value to enable use of min
+					break
+				if(l1[i]!=l2[i]):
+					distance[line2_index] += 1
+				if distance[line2_index]>1:
+					break
+		if (min(distance)==1):
+			differing_index = distance.index(1)
+			word1 = l1
+			word2 = inputs[distance.index(1)]
+			ans = ""
+			for i in range(len(word1)):
+				if word1[i]==word2[i]:
+					ans += word1[i]
+			return ans
 
 if __name__=='__main__':
 	print solvepart1()
