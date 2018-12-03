6eb0f15b32a40eb7fa9bf0e27e201d9fbaf67c1a
Johan Eriksson <imoxus@gmail.com>
2018-12-03 15:43:34 +0100

    day3part1

--- src/day3.py
+++ src/day3.py
@@ -1,9 +1,33 @@
+import re
+
 def solvepart1():
-	freq = 0
-	with open('inputs/day1.txt') as f:
-		for i in f:
-			freq += int(i)
-	return freq
+	shape = 1100 #from visual inpsection of input
+	occupied = [[0 for i in range(shape)] for j in range(shape)]
+	with open('inputs/day3.txt') as f:
+		for line in f:
+			splitlines = line.split()
+			x, y = splitlines[2].split(',')
+			w, h = splitlines[3].split('x')
+			y = y[:-1] # remove the colon
+
+			x,y,w,h = map(int, (x, y, w, h))
+			
+			for i in range(w):
+				for j in range(h):
+					occupied[x+i][y+j] += 1
+
+
+	ans = 0
+	for i in range(shape):
+		for j in range(shape):
+			if occupied[i][j]>1:
+				ans += 1
+
+	return ans
+			
+			
+			
+		
 
 def solvepart2():
 	freq = 0
