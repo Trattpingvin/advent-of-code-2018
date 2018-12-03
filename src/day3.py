3f91e054cba74310423e71df804b22bdf4d23f2e
Johan Eriksson <imoxus@gmail.com>
2018-12-03 15:48:11 +0100

    day3 done

--- src/day3.py
+++ src/day3.py
@@ -25,22 +25,45 @@ def solvepart1():
 
 	return ans
 			
-			
-			
-		
-
 def solvepart2():
-	freq = 0
-	seen_freqs = set([freq])
-	while True:
-		with open('inputs/day1.txt') as f:
-			for i in f:
-				freq += int(i)
-				if freq in seen_freqs:
-					return freq
-				seen_freqs.add(freq)
 
-	return freq
+	def verify_overlap(x, y, w, h):
+		for i in range(w):
+			for j in range(h):
+				if(occupied[x+i][y+j]!=1):
+					return False
+		return True
+		
+	shape = 1100 #from visual inpsection of input
+	occupied = [[0 for i in range(shape)] for j in range(shape)]
+	with open('inputs/day3.txt') as f:
+		for line in f:
+			splitlines = line.split()
+			x, y = splitlines[2].split(',')
+			w, h = splitlines[3].split('x')
+			y = y[:-1] # remove the colon
+
+			x,y,w,h = map(int, (x, y, w, h))
+			
+			for i in range(w):
+				for j in range(h):
+					occupied[x+i][y+j] += 1
+
+	with open('inputs/day3.txt') as f:
+		for line in f:
+			splitlines = line.split()
+			x, y = splitlines[2].split(',')
+			w, h = splitlines[3].split('x')
+			y = y[:-1] # remove the colon
+			claim = splitlines[0]
+
+			x,y,w,h = map(int, (x, y, w, h))
+			if(verify_overlap(x,y,w,h)):
+				return claim
+			
+
+	
+	
 
 if __name__=='__main__':
 	print solvepart1()
