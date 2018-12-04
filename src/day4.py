db66b1726ab3d29c98a807fb5e87aae1d0769858
Johan Eriksson <imoxus@gmail.com>
2018-12-04 17:46:32 +0100

    day4 finished

--- src/day4.py
+++ src/day4.py
@@ -27,7 +27,6 @@ def solvepart1():
 	maxguard = ""
 
 	for guard, minutes in sleeps.items():
-		
 		currentsleep = sum(minutes)
 		if currentsleep>maxsleep:
 			maxsleep = currentsleep
@@ -40,17 +39,39 @@ def solvepart1():
 
 
 def solvepart2():
-	freq = 0
-	seen_freqs = set([freq])
-	while True:
-		with open('inputs/day1.txt') as f:
-			for i in f:
-				freq += int(i)
-				if freq in seen_freqs:
-					return freq
-				seen_freqs.add(freq)
+	with open('inputs/day4.txt') as f:
+		lines = f.readlines()
+			
+	lines.sort()
+	sleeps = {}
+	for line in lines:
+		splitlines = line.split()
+		if "Guard" in line:
+			guard_id = splitlines[3]
+		else:
+			minute = int(splitlines[1][3:5])
+			
+			if "asleep" in line:
+				asleep_time = minute
+				
+			if "wakes" in line:
+				if guard_id not in sleeps:
+					sleeps[guard_id] = [0 for _ in range(60)]
+				for i in range(minute-asleep_time):
+					sleeps[guard_id][asleep_time+i] += 1
 
-	return freq
+
+	
+	maxsleep = 0
+	maxguard = ""
+
+	for guard, minutes in sleeps.items():
+		currentsleep = max(minutes)
+		if currentsleep>maxsleep:
+			maxsleep = currentsleep
+			maxguard = guard
+
+	return sleeps[maxguard].index(max(sleeps[maxguard])) * int(maxguard[1:])
 
 if __name__=='__main__':
 	print solvepart1()
