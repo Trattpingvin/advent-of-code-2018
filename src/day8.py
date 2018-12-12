ce765a84232dd2c3007667cd6b10b520a3773df4
Johan Eriksson <imoxus@gmail.com>
2018-12-12 11:43:38 +0100

    day 8 and 9

--- src/day8.py
+++ src/day8.py
@@ -1,24 +1,77 @@
+import numpy as np
+import pdb
+import StringIO
+import string
+
+
+
+def solve(f): 
+	def rec(data):  #return index of where headers data ends, and sum of metadata
+		if len(data)<2:
+			return 0,0
+		num_children = data[0] 
+		num_metadata = data[1] 
+		total_sum = 0
+		index = 2
+		for c in range(num_children):
+			new_index, new_sum = rec(data[index:])
+			index += new_index
+			total_sum += new_sum
+		for m in range(num_metadata):
+			total_sum += data[index]
+			index += 1
+		return index, total_sum
+	data = np.loadtxt(f, delimiter=' ', dtype=np.int)
+
+	return rec(data)[1]
+
+
 def solvepart1():
-	freq = 0
-	with open('inputs/day1.txt') as f:
-		for i in f:
-			freq += int(i)
-	return freq
+	
+	example = StringIO.StringIO("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2")
+
+	with open('inputs/day8.txt') as f:
+		return solve(f)
+
+	return solve(example)
 
 def solvepart2():
-	freq = 0
-	seen_freqs = set([freq])
-	while True:
-		with open('inputs/day1.txt') as f:
-			for i in f:
-				freq += int(i)
-				if freq in seen_freqs:
-					return freq
-				seen_freqs.add(freq)
 
-	return freq
+	def rec(data):  #return index of where headers data ends, and sum of metadata
+		if len(data)<2:
+			return 0,0
+		num_children = data[0] 
+		num_metadata = data[1] 
+		value = 0
+		index = 2
+		children = []
+
+		for c in range(num_children):
+			child = rec(data[index:])
+			children.append(child)
+			index += child[0]
+			
+		for _ in range(num_metadata):
+			metadata = data[index]
+			if num_children==0:
+				value += metadata
+			else:
+				if metadata>0 and metadata<=len(children):
+					value += children[metadata-1][1]
+			index += 1
+		return index, value
+
+
+
+	f = StringIO.StringIO("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2")
+
+	with open('inputs/day8.txt') as f:
+		data = np.loadtxt(f, delimiter=' ', dtype=np.int)
+	
+	return rec(data)[1]
 
 if __name__=='__main__':
 	print solvepart1()
 	print solvepart2()
+
 
--- src/day9.py
+++ src/day9.py
@@ -1,9 +1,60 @@
+class Node():
+	def __init__(self, val, prevNode=None, nextNode=None):
+		self.prevNode = prevNode
+		self.nextNode = nextNode
+		self.val = val
+
+	def delete(self):
+		self.prevNode.nextNode = self.nextNode
+		self.nextNode.prevNode = self.prevNode
+
+def marblegame(players, marbles): # 0 1 None
+	
+	root = Node(0)
+	root.prevNode = root
+	root.nextNode = root
+	current = root
+
+	scoreboard = [0]*players
+
+	for i in range(1, marbles+1):
+
+		if i%23==0:
+			#backwards seven times
+			for _ in range(7):
+				current = current.prevNode
+			toDelete = current
+			current = current.nextNode
+			scoreboard[i%players] += i + toDelete.val
+			toDelete.delete()
+
+
+		else:
+			#forward twice   0 8 4 2 5 1 6 3 7
+			for _ in range(1): 
+				current = current.nextNode
+			prev_nextNode = current.nextNode
+			new = Node(i, current, prev_nextNode)
+			prev_nextNode.prevNode = new
+			current.nextNode = new
+			current = new
+
+
+		if i%100000==0:
+			print i
+		
+	#ll = root
+	#for i in range(marbles):
+		#print ll.val
+		#ll = ll.nextNode
+	print "\n\n"
+	return max(scoreboard)
+
+
+
 def solvepart1():
-	freq = 0
-	with open('inputs/day1.txt') as f:
-		for i in f:
-			freq += int(i)
-	return freq
+	#return marblegame(493,71863)
+	return marblegame(431,70950*100)
 
 def solvepart2():
 	freq = 0
@@ -20,5 +71,5 @@ def solvepart2():
 
 if __name__=='__main__':
 	print solvepart1()
-	print solvepart2()
+	#print solvepart2()
 
